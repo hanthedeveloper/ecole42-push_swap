3588ac2eeb452531f1a123b2c4894e2896bd69f0
han <bayramincel55@gmail.com>
2026-09-08 15:55:30 +0300

    Add files via upload

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haincel <haincel@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/08 14:33:32 by haincel           #+#    #+#             */
+/*   Updated: 2026/09/08 14:40:13 by haincel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_linkedlist
+{
+	int					data;
+	struct s_linkedlist	*next;
+	struct s_linkedlist	*prev;
+}	t_linkedlist;
+
+void	push(int data, t_linkedlist *stack);
+
+#endif
---
+++ stack_op.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_op.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haincel <haincel@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/08 14:37:55 by haincel           #+#    #+#             */
+/*   Updated: 2026/09/08 15:53:45 by haincel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <stdlib.h>
+
+/* push: stack data structer mantıgı dolayısıyla,
+cagırıldıgında verılen data ıle yenı node olusturarak
+verılen lınked lıstın sureklı basına ekleme yapar*/
+void	push(int data, t_linkedlist **stack)
+{
+
+	t_linkedlist	*newnode;
+
+	if (!stack)
+		return ;
+	newnode = (t_linkedlist *)malloc(sizeof(t_linkedlist));
+	if (!newnode)
+		return ;
+	newnode->data = data;
+	newnode->prev = NULL;
+	newnode->next = *stack;
+	if (*stack)
+		(*stack)->prev = newnode;
+	*stack = newnode;
+}
+
+/* lınked lıstın tepesındekı datanın stackten cıkarılması ıcındır.
+boylece yenı data bı alttakı olur.
+or: stack bası -> 1 2 3 4 poptan sonra stack bası -> 2 3 4 */
+int	pop(t_linkedlist **stack)
+{
+	int				popdata;
+	t_linkedlist	*temp;
+
+	if (!stack || !*stack)
+		return (0);
+	popdata = (*stack)->data;
+	temp = *stack;
+	*stack = temp->next;
+	if (*stack)
+		(*stack)->prev = NULL;
+	free(temp);
+	return (popdata);
+}
+
+/* stack yapısındakı en tepedekı datayı dondurur */
+int	peek(t_linkedlist **stack)
+{
+	if (!stack || !*stack)
+		return (0);
+	return ((*stack)->data);
+}
