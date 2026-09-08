c5f5e38f1f80434c63ae7f78fa694fb4df89effb
han <bayramincel55@gmail.com>
2026-09-08 16:29:19 +0300

    stack op done. subject op/2
    linked list uzerınden push pop peek stack operatorlerı yazıldı bknz stack_op.c. 
    subjectte yer alan sa sb etc. ıcın genel fonksıyonlar yazıldı (rotateler harıc) bknz subject_op_utils.c
    headera prototıpler eklendı bknz. push_swap.h

--- subject_op.c
+++ subject_op.c
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   subject_op.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: haincel <haincel@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/08 14:33:32 by haincel           #+#    #+#             */
-/*   Updated: 2026/09/08 16:25:31 by haincel          ###   ########.fr       */
+/*   Created: 2026/09/08 16:26:53 by haincel           #+#    #+#             */
+/*   Updated: 2026/09/08 16:27:22 by haincel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-typedef struct s_linkedlist
-{
-	int					data;
-	struct s_linkedlist	*next;
-	struct s_linkedlist	*prev;
-}	t_linkedlist;
-
-void	push(int data, t_linkedlist **stack);
-int		pop(t_linkedlist **stack);
-int		peek(t_linkedlist **stack);
-void	swap(t_linkedlist **stack);
-void	move(t_linkedlist **from, t_linkedlist **to);
-
-#endif
+// sa sb ss pa pb ra rb rr rra rrb rrr
