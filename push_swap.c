d83efcf3e9f2adedb646615d1da7289557c9e0fa
han <bayramincel55@gmail.com>
2026-09-14 23:45:38 +0300

    bleeh :D

--- push_swap.c
+++ push_swap.c
@@ -47,7 +47,8 @@ static void	call_flag(int flag_index, int bench, t_linkedlist **a,
 	temp->disorder = disorder;
 	if (disorder == 0.00)
 	{
-		if (flag_index == 0) //
+		if (flag_index == 0)
+			temp->adaptive_strategy = "O(1)";
 		if (bench == 1)
 			print_bench();
 		return ;
